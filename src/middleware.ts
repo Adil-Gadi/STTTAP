45fe378408cfef4dda2950832f5ca3731581a626
Adil Gadi <adil@gadifamily.com>
2023-10-21 14:15:51 -0700

    upgrades to Astro 3.0

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
-import { defineMiddleware } from 'astro/middleware';
-import { isAuth } from './auth/isAuth';
+import { defineMiddleware } from "astro/middleware";
+import { isAuth } from "./auth/isAuth";
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware(async (context, next) => {
-	//Authorization middleware for server-side rendering
-	const auth = await isAuth(
-		context.cookies.get('my-refresh-token').value,
-		context.cookies.get('my-access-token').value
-	);
+  //Authorization middleware for server-side rendering
+  const auth = await isAuth(
+    context.cookies.get("my-refresh-token")?.value,
+    context.cookies.get("my-access-token")?.value,
+  );
 
-	context.locals.loggedIn = auth ? true : false;
-	if (auth) {
-		context.locals.session = auth;
-	}
+  context.locals.loggedIn = auth ? true : false;
+  if (auth) {
+    context.locals.session = auth;
+  }
 
-	return next();
+  return next();
 });
